2007f29af12848d79978bb2b5299cecc764fddbf
Luna <imlvnaa@gmail.com>
2024-02-05 14:17:14 -0500

    forgot to include common repo

---
+++ Packages/gay.lvna.common/README.md
@@ -0,0 +1,1 @@
+# gay.lvna.common by luna
---
+++ Packages/gay.lvna.common/Runtime/Colored.cs
@@ -0,0 +1,13 @@
+﻿
+using UdonSharp;
+
+namespace gay.lvna.common.udon
+{
+    public class Colored : UdonSharpBehaviour
+    {
+        public static string C(string text, string color)
+        {
+            return "<color=" + color + ">" + text + "</color>";
+        }
+    }
+}
